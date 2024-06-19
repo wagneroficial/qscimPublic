13307785b0ffa7eedb666f8b8b3e1fca64353815
wagneroficial <wagner.sampaio@discente.ufopa.edu.br>
2024-06-19 00:47:09 -0300

    Automatic upload of folder at 2024/06/19T03-47-09

--- scimgateway/lib/adapters/data/caches.js
+++ scimgateway/lib/adapters/data/caches.js
@@ -1,3 +1,11 @@
-const caches = [];
+const caches = [{
+  "port": "8880",
+  "method": "GET",
+  "expires_in": 60,
+  "mapping": [],
+  "auth": {
+    "type": "none"
+  }
+}];
             
 module.exports = { caches };
